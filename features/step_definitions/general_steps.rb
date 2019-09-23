89547522c32725a85b6983027cb6b4354895095a
dustinsison <dustin.sison@moovweb.com>
2019-09-23 15:40:21 -0700

    TravisCI Test 1.5

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -7,6 +7,8 @@ require 'selenium-webdriver'
 options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
+  options.add_argument("--window-size=1920,1080")
+  options.add_argument("--start-maximized")
   options.add_argument('--remote-debugging-port=9222')
 
 WebDriver = Selenium::WebDriver.for :chrome, options: options
