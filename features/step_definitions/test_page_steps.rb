82e9e347967d3769df3d76be7818df4752933a5c
Dustin Sison <dustin.sison@moovweb.com>
2020-01-02 16:16:34 -0800

    Partially added Test Page steps

---
+++ features/step_definitions/test_page_steps.rb
@@ -0,0 +1,40 @@
+Then(/^user can see test_page elements$/) do
+  p "Checking for test page HTML elements"
+  WebDriver.find_element(:id, "input_field")
+  p "- Found input field element."
+  WebDriver.find_element(:id, "single_sel_field")
+  p "- Found selection field (single) element."
+  WebDriver.find_element(:id, "multi_sel_field")
+  p "- Found selection field (multiple) element."
+  WebDriver.find_element(:id, "button")
+  p "- Found button element."
+  WebDriver.find_element(:id, "dropdown")
+  p "- Found dropdown field element."
+  WebDriver.find_element(:id, "output_field")
+  p "- Found output field element."
+  p "All test page elements present."
+end
+
+When(/^user clicks on "([^"]*)" page_element$/) do |id|
+  WebDriver.find_element(:id, id).click
+  p id + " element clicked."
+end
+
+And(/^user inputs "([^"]*)" on "([^"]*)" page_element$/) do |text, id|
+  WebDriver.find_element(:id, id).send_keys(text)
+  p "Entered " + text + " into " + id + " page element."
+end
+
+Then(/^user can see "([^"]*)" on "([^"]*)" page_element$/) do |text, id|
+  found_text = WebDriver.find_element(:id, id).text
+  if found_text == text
+    p "Text in element matches previous input."
+  else
+    p "Text in element does not match previous input."
+  end
+end
+
+
+And(/^user clicks on "([^"]*)" in "([^"]*)" page_element$/) do |option, id|
+  WebDriver.find_element(:xpath, "//select[id='"+id+"']/option[text='"+option+"']").click
+end
---
+++ features/testpage.feature
@@ -0,0 +1,25 @@
+Feature: Test Page
+
+
+  Scenario: As a user I can utilize all elements present on the Automation QA Test Page
+    Given user is on "automated-qa-test-page" page
+    Then user can see test_page elements
+    When user clicks on "input_field" page_element
+    And user inputs "test" on "input_field" page_element
+    Then user can see "test" on "input_field" page_element
+    When user clicks on "single_sel_field" page_element
+    And user clicks on "five" in "single_sel_field" page_element
+    Then user can see "test" selected on "single_sel_field" page_element
+    When user clicks on "multi_sel_field" page_element
+    And user clicks on "four" in "multi_sel_field" page_element
+    And user clicks on "five" in "multi_sel_field" page_element
+    Then user can see "four" and "five" selected on "multi_sel_field" page_element
+    When user clicks on "button" page_element
+    When user clicks on "dropdown" page_element
+    And user inputs "test" on "dropdown" page_element
+    And user clicks "dropdown_three" on "dropdown" page_element
+    Then user can see "dropdown_three" selected on "dropdown" page_element
+    When user clicks on "output_field" page_element
+    And user inputs "50" on "output_field" page_element
+    And user drags slider right on "output_field" page_element
+    Then user can see "150" on "output_field" page_element
